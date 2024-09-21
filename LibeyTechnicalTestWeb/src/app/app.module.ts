bb903e68ff895b0e56e59ef738cdf179785fd793
Jair0xGS <jairguevara0x00@gmail.com>
2024-09-21 14:21:36 -0500

    feat:add search box

--- LibeyTechnicalTestWeb/src/app/app.module.ts
+++ LibeyTechnicalTestWeb/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { RouterModule } from "@angular/router";
 import { UserModule } from "./User/user/user.module";
+import { FormsModule } from "@angular/forms";
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, RouterModule,HttpClientModule , UserModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, RouterModule,HttpClientModule , UserModule,FormsModule],
+    
 	providers: [],
 	bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
